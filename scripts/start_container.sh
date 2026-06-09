ebce6f4a24a9cc329575d7a3759eebfb81af5b96
abhatt014 <65420762+abhatt014@users.noreply.github.com>
2026-06-09 18:48:30 +0530

    fi

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 # Pull the Docker image from Docker Hub
-docker pull amitfreeze/cloudnative-jan:lates
+docker pull amitfreeze/cloudnative-jan:latest
 # Run the Docker image as a container
-docker run -d -name pyapp -p 5000:5000 cloudnative-jan:lates
+docker run -d --name pyapp -p 5000:5000 amitfreeze/cloudnative-jan:latest
